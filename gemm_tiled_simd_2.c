53946f4e863f1d48eb2d185d9f2e6bd84a949c16
AaronWang04 <aaronw23880@gmail.com>
2024-12-06 22:49:06 -0500

    .

---
+++ gemm_tiled_simd_2.c
@@ -0,0 +1,88 @@
+// clang -O3 -march=native -ffast-math gemm_tiled_simd_2.c && ./a.out
+// same speed as regular gemm_simd, not sure how to achieve cache coherency, need to mess around?
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdalign.h>
+#include <math.h>
+#include <stdint.h>
+#include <immintrin.h> // For SIMD intrinsics
+#include <time.h>
+#include <assert.h>
+
+#define N 1024
+#define BLOCK_SIZE 8
+#define REGISTER_SIZE 8
+
+uint64_t nanos(){
+    struct timespec t;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &t);
+    return (uint64_t)t.tv_sec * 1000000000 + (uint64_t)t.tv_nsec;
+}
+
+
+void print_float(float* values) {
+    printf("float [%.6f, %.6f, %.6f, %.6f, %.6f, %.6f, %.6f, %.6f]\n",
+           values[0], values[1], values[2], values[3],
+           values[4], values[5], values[6], values[7]);
+}
+
+void print_m256(__m256 reg) {
+    float* values = (float*)&reg;
+    printf("[%.6f, %.6f, %.6f, %.6f, %.6f, %.6f, %.6f, %.6f]\n",
+           values[0], values[1], values[2], values[3],
+           values[4], values[5], values[6], values[7]);
+}
+
+float A[N][N];
+float AT[N][N];
+float B[N][N];
+float BT[N][N];
+float C[N][N];
+float verify[N][N];
+
+int main(){
+    
+    assert (N % 8 == 0);
+
+    FILE *f = fopen("/tmp/gemm", "rb");
+    if (f == NULL) {
+        printf("Error opening file!\n");
+        return -1;
+    }
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wunused-result"
+    fread(A, 1, sizeof(float)*N*N, f);
+    fread(B, 1, sizeof(float)*N*N, f);
+    fread(verify, 1, sizeof(float)*N*N, f);
+    fclose(f);
+    #pragma GCC diagnostic pop
+
+    double flops = 2.0 * N * N * N * 1e-9;
+    uint64_t start = nanos();
+
+    // swizzling for transpose
+    for (int i = 0; i < N; i+=8) {
+        for (int j = 0; j < N; j++) {
+            for (int l = 0; l < 8; l++) {
+                BT[i + l][j] = B[j][i + l];
+                AT[i + l][j] = A[j][i + l];
+            }
+        }
+    }
+
+
+    uint64_t end = nanos();
+    double gflops = (double)flops / (double)((end - start) * 1e-9);
+    printf("GFLOP/s: %f\n", gflops);
+
+    for (int k = 0; k < N*N; k++) {
+        if (fabsf(C[0][k]- verify[0][k]) > 1e-3) {
+            printf("Verification failed\n");
+            return 1;
+        }
+    }
+    printf("Verfication successful\n");
+
+    return 0;
+}
